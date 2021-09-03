803b466db7afe88756bd1f669baa4c1a92705dd2
Tyfani21 <tyfani.abreu@gmail.com>
2021-09-03 15:04:29 -0400

    musicas
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

---
+++ controllers/musicas.controllers.js
@@ -0,0 +1,11 @@
+function validaAuthAPI(key) {
+    const chave = 0
+    
+    if(key === chave){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+module.exports = validaAuthAPI;
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 const Conn = require('./models/conn/conn');
+
+Conn("localhost",27017,"musicas");
+
 const port = 3000;
 
-const musica = require('')
+const musicasRouter = require('./routers/musicas.routers');
+app.use('/musicas',musicasRouter);
+
+const filmeRouter = require('./routers/filmes.routers');
+app.use('/filmes',filmeRouter);
+
+app.listen(port, ()=> {
+  console.info(`Servidor rodando na porta ${port}`);
+})

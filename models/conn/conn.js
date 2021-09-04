f6eeef525f40b2842a068d1dc3078e33f597a758
Tyfani21 <tyfani.abreu@gmail.com>
2021-09-03 20:43:02 -0400

    mongodb
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

--- models/conn/conn.js
+++ models/conn/conn.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 // url de conexao -> mongodb://servidor:porta/nome do banco
 // useNewUrlParser --> Para que o Moongoose use o novo sistema de Url
 // useUnifiedTopology --> Mecanismo de monitoramento de Banco de Dados
+const username = 'db_adm'
+const password = 'q8nLNP95iveKT4W'
+
+
 function Conn(url,porta,banco){
-    mongoose.connect(`mongodb://${url}:${porta}/${banco}`, {
+    mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.u90bu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
-        console.log(`MongoDB connected -> mongodb://${url}:${porta}/${banco}`);
+        console.log(`MongoDB connected -> mongodb+srv://${username}:${password}@cluster0.u90bu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
     }).catch((err) => {
         console.error(err);
     });
